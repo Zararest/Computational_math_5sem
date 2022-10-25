e36b95a24838ab59c313f0989a5e032ee7869dfb
UwU <iishumakov@gmail.com>
2022-10-26 01:01:54 +0300

    Финальная версия

--- Lab_3/Calc.py
+++ Lab_3/Calc.py
@@ -3,7 +3,7 @@
 from distutils.command.build_scripts import first_line_re
 import numpy as np    
 
-epsilon = 0.001
+epsilon = 0.00001
 
 def ostanov(prev_X, cur_X, q):
     return (np.abs(prev_X - cur_X) / (1 - q)) >= epsilon
@@ -42,8 +42,37 @@ def calc_equation():
     ans.append(calc_right_X())
     return ans
 
+#--------------
+
+def F_1(val):
+    return np.sin(val[0]) - val[1] - 1.32
+
+def F_2(val):
+    return np.cos(val[1]) - val[0] + 0.85
+
+F = [F_1, F_2]
+
+def system_ostanov(cur_result):
+
+    return max(np.abs(F_1(cur_result)), np.abs(F_2(cur_result))) >= epsilon
+
+def rev_J_F(val):
+    fir_val = np.cos(val[0]) * F[0](val) - F[1](val)
+    sec_val = -F[0](val) - np.sin(val[1]) * F[1](val)
+    return np.array([fir_val, sec_val])
+
+def calc_system():
+    cur_val = np.array([1.8, -0.1])
+    prev_val = cur_val
+    while system_ostanov(cur_val):
+        prev_val = cur_val
+        cur_val = prev_val - rev_J_F(prev_val)
+    return cur_val
+        
+
 def main():
     print('Result equation', calc_equation())
+    print('Result system', calc_system())
 
 if __name__ == '__main__':
     main()
